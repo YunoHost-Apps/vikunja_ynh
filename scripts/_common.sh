4e4baaba22c10c92a761f66de9c841b3fbc1ad36
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-02 19:15:23 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql"
+#REMOVEME? pkg_dependencies="postgresql"
 
 #=================================================
 # PERSONAL HELPERS
